4d60886d6a0682e486ea7a63d8c0cb5c60ce020e
i74ifa <i74ifa@gmail.com>
2021-10-20 09:25:57 +0300

    phpDoc and add locale static function

--- src/Static/Locale.php
+++ src/Static/Locale.php
@@ -5,19 +5,38 @@ namespace I74ifa\Locale\Static;
 use I74ifa\Locale\Locale as LocaleLocale;
 
 /**
- * give me directory languages and locale name
+ * give me directory languages and locale name\
+ * start your project or file set $dir and $lang
  */
 class Locale
 {
-    public static $dir;    
-    public static $lang;
+    /**
+     * directory to locales
+     * @var string $dir
+     */
+    public static string $dir;
 
-    public static function get($field)
+    /**
+     * locale short name like 'ar' || 'en'
+     * @var string $locale
+     */
+    public static string $locale;
+
+    /**
+     * getting the words
+     * @param string $field
+     */
+    public static function get(string $field): string
     {
-        $lang = new LocaleLocale(self::$lang, self::$dir);
+        $lang = new LocaleLocale(self::$locale, self::$dir);
         if (array_key_exists($field, $lang->locale)){
             return $lang->locale[$field];
         }
         return $field;
+    }
+
+    public static function locale()
+    {
+        return self::$locale;
     }
 }
