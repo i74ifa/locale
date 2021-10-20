b271e7dfb1161f339b6901490b6089426223fcc7
i74ifa <i74ifa@gmail.com>
2021-10-20 09:36:26 +0300

    d

--- src/Static/Locale.php
+++ src/Static/Locale.php
@@ -6,7 +6,7 @@ use I74ifa\Locale\Locale as LocaleLocale;
 
 /**
  * give me directory languages and locale name\
- * start your project or file set $dir and $lang
+ * start your project or file set $dir and $locale
  */
 class Locale
 {
@@ -28,9 +28,9 @@ class Locale
      */
     public static function get(string $field): string
     {
-        $lang = new LocaleLocale(self::$locale, self::$dir);
-        if (array_key_exists($field, $lang->locale)){
-            return $lang->locale[$field];
+        $locale = new LocaleLocale(self::$locale, self::$dir);
+        if (array_key_exists($field, $locale->locale)){
+            return $locale->locale[$field];
         }
         return $field;
     }
--- tests/StaticLangTest.php
+++ tests/StaticLangTest.php
@@ -10,7 +10,7 @@ class StaticLangTest extends TestCase
     /** @test  */
     public function getting_word_from_static_lang_class()
     {
-        Locale::$lang = 'ar';
+        Locale::$locale = 'ar';
         Locale::$dir = __DIR__ . '/lang';
         $this->assertEquals('مرحبا', Locale::get('welcome'));
     }
@@ -18,7 +18,7 @@ class StaticLangTest extends TestCase
     /** @test  */
     public function if_key_not_exist_return_same_key()
     {
-        Locale::$lang = 'ar';
+        Locale::$locale = 'ar';
         Locale::$dir = __DIR__ . '/lang';
         $this->assertEquals('not world', Locale::get('not world'));
         $this->assertEquals('not word exist', Locale::get('not word exist'));
