323f4e7a52e1f4ec5f0a14c4e9019f5799899b98
i74ifa <i74ifa@gmail.com>
2021-11-08 13:18:54 +0300

    change name from locale to move

---
+++ src/Setup.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace I74ifa\Locale;
+
+class Setup
+{
+    /**
+     * dir for all files languages
+     * 
+     * @var string $dirLanguages
+     */
+    public $dirLang;
+
+    
+    public $dirName;
+    
+    public $lang;
+
+    public $locale;
+
+    public function __construct(string $lang, $dirName)
+    {
+        $this->lang = $lang;
+        $this->dirName = $dirName;
+        $this->dir();
+        $this->openFile();
+        
+    }
+
+    /**
+     * convert string to real dir
+     */
+    protected function dir()
+    {
+        if (!is_dir($this->dirName)){
+            throw new \Exception("not directory");
+        }
+        $this->dirLang = dir($this->dirName); 
+    }
+
+    public function getFile()
+    {
+        return (new File($this->lang, $this->dirLang->path))->get();
+    }
+
+    protected function openFile()
+    {
+        $this->locale = include($this->getFile());
+    }
+
+    public function getDir()
+    {
+        return $this->dirLang;
+    }
+    
+    public function get(string $field)
+    {
+        if (array_key_exists($field, $this->locale)){
+            return $this->locale[$field];
+        }
+        return $field;
+    }
+
+
+}
